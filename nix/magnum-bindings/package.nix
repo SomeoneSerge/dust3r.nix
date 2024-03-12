0fed3dbc45a07b12e0c20e21d689d66593e30759
Someone Serge <sergei.kozlukov@aalto.fi>
2024-03-12 23:39:19 +0000

    magnum-bindings: build the python extension

--- nix/magnum-bindings/package.nix
+++ nix/magnum-bindings/package.nix
@@ -1,18 +1,22 @@
 {
+  buildPythonPackage,
   lib,
   stdenv,
   fetchFromGitHub,
   cmake,
+  ninja,
   corrade,
   magnum,
   libGL,
-  pybind11,
   xorg,
+  pybind11,
+  setuptools,
 }:
 
-stdenv.mkDerivation rec {
+buildPythonPackage rec {
   pname = "magnum-bindings";
   version = "unstable-2024-03-08";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "mosra";
@@ -21,7 +25,11 @@ stdenv.mkDerivation rec {
     hash = "sha256-JKcPiI28O3Or3c9QmA1GsLNdaNy3voP5z6/DQenRh6k=";
   };
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [
+    cmake
+    ninja
+    setuptools
+  ];
 
   buildInputs = [
     corrade
@@ -30,7 +38,22 @@ stdenv.mkDerivation rec {
     pybind11
   ] ++ lib.optionals stdenv.hostPlatform.isLinux [ xorg.libX11 ];
 
-  cmakeFlags = [ (lib.cmakeBool "MAGNUM_WITH_PYTHON" true) ];
+  cmakeFlags = [ "-GNinja" (lib.cmakeBool "MAGNUM_WITH_PYTHON" true) ];
+
+  # Runs pre/postBuild hooks twice...
+  buildPhase = ''
+    runPhase ninjaBuildPhase
+
+    # "$sourceRoot/build/src/python"
+    cd src/python
+    runPhase pypaBuildPhase
+  '';
+
+  installPhase = ''
+    runPhase pypaInstallPhase
+
+    (cd ../.. ; runPhase ninjaInstallPhase)
+  '';
 
   meta = with lib; {
     description = "Bindings of the Magnum C++11 graphics engine into other languages";
