cf159ccbab05b6b023b8f5050572adfa21b30a34
Someone Serge <sergei.kozlukov@aalto.fi>
2024-03-13 20:34:21 +0000

    nix: advertise cuda cache

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "Description for the project";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://cuda-maintainers.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
+  };
+
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
